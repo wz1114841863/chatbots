b60b2dca25734bd1c21e6a429c688cc3150789af
wz1114841863 <18801383735@163.com>
2025-04-04 13:58:04 +0800

    use textSpliter

--- langchain_api.py
+++ langchain_api.py
@@ -1,7 +1,10 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from API_KEY import DEEPSEEK_API
 
 BASE_URL = "https://api.deepseek.com"
@@ -44,5 +47,31 @@ def prompt_template():
     print(prompt.to_messages())
 
 
+def document_load():
+    """加载文件, 生成document对象"""
+    file_path = "./tmp/nke-10k-2023.pdf"
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    print(len(docs))
+    # print(f"{docs[0].page_content[:200]}\n")
+    # print(docs[0].metadata)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        add_start_index=True,
+    )
+    all_splits = text_splitter.split_documents(docs)
+    print(len(all_splits))
+
+    # 如何获取deepseek embedding模型.
+    # embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=API_KEY)
+    # vector_1 = embeddings.embed_query(all_splits[0].page_content)
+    # vector_2 = embeddings.embed_query(all_splits[1].page_content)
+    # assert len(vector_1) == len(vector_2)
+    # print(f"Generated vectros of length {len(vector_1)}")
+    # print(vector_1[0])
+
+
 if __name__ == "__main__":
-    prompt_template()
+    document_load()
